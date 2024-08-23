10718f16acfb65aac00fd7a56cfde471c5b5a4e6
its-christoph <christoph.koscheck@gmail.com>
2024-08-23 13:49:21 +0200

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -2,23 +2,18 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from PIL import Image
-from skimage import io, transform, feature
+from skimage import io, feature
 from skimage.transform import resize
-from sklearn.preprocessing import normalize
 import seaborn as sns
-from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 import tensorflow as tf
-from sklearn.cluster import KMeans
 
 resolution = 128
 sample_image_num = 3
 
-# Function Calls
+# Funktionsaufruf
 def main():
-    # Load training and testing datasets
+    # Trainings- und Testdaten laden
     ROOT_PATH = "./"
     train_data_dir = os.path.join(ROOT_PATH, "Training")
     test_data_dir = os.path.join(ROOT_PATH, "Testing")
@@ -26,52 +21,46 @@ def main():
     train_images, train_labels, train_paths = load_data(train_data_dir)
     test_images, test_labels, test_paths = load_data(test_data_dir)
 
-    # Create a DataFrame for labels and paths
+    # DataFrame für Pfade und ClassIds erstellen
     labels_df = pd.DataFrame({
         'Path': np.concatenate([train_paths, test_paths]),
         'ClassId': np.concatenate([train_labels, test_labels])
     })
 
-    # # Display the first few rows of the dataset
-    print("First few rows of the dataset:")
-    print(labels_df.head())
-
-    # # Display the distribution of classes
-    print("\nDistribution of classes in the dataset:")
+    # Ausgabe der Klassenverteilung
+    print("\nVerteilung der Klassen im Datensatz:")
     print(labels_df['ClassId'].value_counts())
 
-    # Plot the distribution of classes
+    # Visualisierung der Klassenverteilung
     plot_class_distribution(labels_df)
 
-    # Plot images for classes
+    # Visualisierung von Stichproben der Bilder jeder Klasse
     plot_images_for_classes(labels_df, train_data_dir, test_data_dir, n_images=sample_image_num)
 
-    # Get and summarize image dimensions
+    # Berechnung und Ausgabe des Seitenverhältnisses der Bilder und Pixel-Anzahl
     dimensions_df = get_image_stats(train_data_dir, test_data_dir, labels_df)
 
-    # Plot image dimension and aspect ratio distribution
+    # Visualisierung der Verteilung der Bildgrößen und Seitenverhältnisse
     plot_image_stats(dimensions_df)
 
-    # Split the data into training and testing sets
-    train_df, test_df = split_data(labels_df)
-
-    # Calculate and plot vibrant colors
+    # Berechnung der dominierenden Farben
     vibrant_colors = calculate_vibrant_colors(labels_df, train_data_dir, test_data_dir)
     labels_df['VibrantColor'] = vibrant_colors
 
-    # Plot random images with identified vibrant color
+    # Visualisierung von Stichproben mit ihren dominierenden Farben
     plot_vibrant_color_images(labels_df, train_data_dir, test_data_dir, vibrant_colors, dimensions_df)
 
-    # Extract and plot color histograms
+    # Extrahieren und Visualisieren von Farbhistogrammen (nur zur Veranschaulichung)
     plot_color_histograms(labels_df, train_data_dir, test_data_dir)
 
-    # Extract and plot edges
+    # Extrahieren und Visualisieren von Kantenbildern (nur zur Veranschaulichung)
     plot_edges(labels_df, train_data_dir, test_data_dir)
 
-    # Augment and plot images
+    # Visualisierung von augmentierten Bildern (nur zur Veranschaulichung)
     augment_and_plot_images(labels_df, train_data_dir, test_data_dir)
 
-# Function Definitions
+## Funktionen
+# Laden der Trainings- und Testdaten
 def load_data(data_dir, target_size=(resolution, resolution)):  
     images = []
     labels = []
@@ -85,17 +74,19 @@ def load_data(data_dir, target_size=(resolution, resolution)):
                     image_resized = resize(image, target_size, anti_aliasing=True)
                     images.append(image_resized)
                     labels.append(int(class_dir))
-                    paths.append(os.path.join(class_dir, f))  # Save relative path for later
+                    paths.append(os.path.join(class_dir, f))
     return np.array(images), np.array(labels), np.array(paths)
 
+# Visualisierung der Klassenverteilung
 def plot_class_distribution(labels_df):
     plt.figure(figsize=(12, 6))
     sns.countplot(x='ClassId', data=labels_df, palette='viridis')
-    plt.title('Distribution of Traffic Sign Classes')
-    plt.xlabel('Class ID')
-    plt.ylabel('Number of Images')
+    plt.title('Verteilung der Verkehrszeichenklassen')
+    plt.xlabel('Klassen-ID')
+    plt.ylabel('Anzahl der Bilder')
     plt.show()
 
+# Visualisierung von Stichproben der Bilder jeder Klasse
 def plot_images_for_classes(labels_df, train_data_dir, test_data_dir, n_images):
     unique_classes = labels_df['ClassId'].unique()
     n_classes = len(unique_classes)
@@ -113,7 +104,7 @@ def plot_images_for_classes(labels_df, train_data_dir, test_data_dir, n_images):
             elif os.path.exists(img_path_test):
                 img_path = img_path_test
             else:
-                print(f"Image not found: {class_images[j]}")
+                print(f"Bild nicht gefunden: {class_images[j]}")
                 continue
             try:
                 img = Image.open(img_path)
@@ -123,13 +114,14 @@ def plot_images_for_classes(labels_df, train_data_dir, test_data_dir, n_images):
                 if j == 0:
                     plt.title(f'Class {class_id}')
             except FileNotFoundError as e:
-                print(f"Error loading image: {e}")
+                print(f"Fehler beim Laden des Bildes: {e}")
                 continue
 
     plt.tight_layout()
     plt.subplots_adjust(top=0.95, bottom=0.0, left=0.2, right=0.8, hspace=0.87, wspace=0.2)
     plt.show()
 
+# Berechnung und Ausgabe des Seitenverhältnisses der Bilder und Pixel-Anzahl
 def get_image_stats(train_data_dir, test_data_dir, labels_df):
     dimensions = []
     for img_path in labels_df['Path'].values:
@@ -140,69 +132,44 @@ def get_image_stats(train_data_dir, test_data_dir, labels_df):
         elif os.path.exists(img_path_test):
             full_img_path = img_path_test
         else:
-            print(f"Image not found: {img_path}")
+            print(f"Bild nicht gefunden: {img_path}")
             continue
         try:
             img = Image.open(full_img_path)
             dimensions.append(img.size)
         except FileNotFoundError as e:
-            print(f"Error loading image: {e}")
+            print(f"Fehler beim Laden des Bildes: {e}")
             continue
     
     dimensions_df = pd.DataFrame(dimensions, columns=['Width', 'Height'])
     dimensions_df['AspectRatio'] = dimensions_df['Width'] / dimensions_df['Height']
     
-    print("\nSummary statistics for image dimensions:")
+    print("\nZusammenfassende Statistiken für Bilddimensionen:")
     print(dimensions_df.describe())
     
     return dimensions_df
 
+# Visualisierung der Verteilung der Bildgrößen und Seitenverhältnisse
 def plot_image_stats(dimensions_df):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-    sns.histplot(dimensions_df['Width'], kde=True, label='Width', ax=ax1)
-    sns.histplot(dimensions_df['Height'], kde=True, label='Height', ax=ax1)
-    ax1.set_xlabel('Dimension in pixels')
-    ax1.set_ylabel('Image count')
-    ax1.set_title('Distribution of Image Width and Height')
+    sns.histplot(dimensions_df['Width'], kde=True, label='Breite', ax=ax1)
+    sns.histplot(dimensions_df['Height'], kde=True, label='Höhe', ax=ax1)
+    ax1.set_xlabel('Dimension in Pixel')
+    ax1.set_ylabel('Anzahl der Bilder')
+    ax1.set_title('Verteilung von Bildbreite und -höhe')
     ax1.legend()
 
-    sns.histplot(dimensions_df['AspectRatio'], kde=True, label='Aspect Ratio', ax=ax2, color='r')
-    ax2.set_xlabel('Aspect Ratio')
-    ax2.set_ylabel('Image count')
-    ax2.set_title('Distribution of Image Aspect Ratio')
+    sns.histplot(dimensions_df['AspectRatio'], kde=True, label='Bildseitenverhältnis', ax=ax2, color='r')
+    ax2.set_xlabel('Bildseitenverhältnis')
+    ax2.set_ylabel('Anzahl der Bilder')
+    ax2.set_title('Verteilung des Bildseitenverhältnisses')
     ax2.legend()
 
     plt.tight_layout()
     plt.show()
 
-def split_data(labels_df):
-    train_df, test_df = train_test_split(labels_df, test_size=0.2, stratify=labels_df['ClassId'], random_state=42)
-
-    print("\nNumber of training samples:", len(train_df))
-    print("Number of testing samples:", len(test_df))
-
-    # Save the training and test sets for future use
-    train_df.to_csv('train_labels.csv', index=False)
-    test_df.to_csv('test_labels.csv', index=False)
-
-    # Visualize class imbalance
-    plt.figure(figsize=(12, 6))
-    sns.countplot(x='ClassId', data=labels_df, palette='viridis')
-    plt.title('Class Distribution with Imbalance')
-    plt.xlabel('Class ID')
-    plt.ylabel('Number of Images')
-    plt.xticks(rotation=90)
-    plt.show()
-
-    # Check the ratio of the most frequent class to the least frequent class
-    class_counts = labels_df['ClassId'].value_counts()
-    print(f"Most frequent class has {class_counts.max()} samples.")
-    print(f"Least frequent class has {class_counts.min()} samples.")
-    print(f"Ratio (most/least): {class_counts.max() / class_counts.min():.2f}")
-
-    return train_df, test_df
-
+# Berechnung der dominierenden Farben
 def calculate_vibrant_colors(labels_df, train_data_dir, test_data_dir):
     def calculate_vibrant_color(image):
         image = image.convert('RGB')
@@ -210,7 +177,7 @@ def calculate_vibrant_colors(labels_df, train_data_dir, test_data_dir):
         np_image = np_image.reshape(-1, 3)
         avg_color = np.mean(np_image, axis=0)
         max_color = np.argmax(avg_color)
-        color_names = ['Red', 'Green', 'Blue', 'Yellow']
+        color_names = ['Rot', 'Grün', 'Blau']
         vibrant_color = color_names[max_color]
         return vibrant_color
 
@@ -234,6 +201,7 @@ def calculate_vibrant_colors(labels_df, train_data_dir, test_data_dir):
 
     return vibrant_colors
 
+# Visualisierung von Stichproben mit ihren dominierenden Farben
 def plot_vibrant_color_images(labels_df, train_data_dir, test_data_dir, vibrant_colors, dimensions_df):
     sample_images = labels_df.sample(5)
     plt.figure(figsize=(12, 6))
@@ -246,7 +214,7 @@ def plot_vibrant_color_images(labels_df, train_data_dir, test_data_dir, vibrant_
         elif os.path.exists(img_path_test):
             full_img_path = img_path_test
         else:
-            print(f"Image not found: {img_path}")
+            print(f"Bild nicht gefunden: {img_path}")
             continue
         try:
             img = Image.open(full_img_path)
@@ -254,32 +222,37 @@ def plot_vibrant_color_images(labels_df, train_data_dir, test_data_dir, vibrant_
             plt.subplot(2, 5, idx+1)
             plt.imshow(img)
             plt.axis('off')
-            plt.title(f'Vibrant Color: {vibrant_color}')
+            plt.title(f'Dominierende Farbe: {vibrant_color}')
         except FileNotFoundError as e:
-            print(f"Error loading image: {e}")
+            print(f"Fehler beim Laden des Bildes: {e}")
 
     plt.tight_layout()
     plt.show()
 
-    # Add vibrant color to labels_df
-    color_mapping = {'Red': 0, 'Green': 1, 'Blue': 2, 'Yellow': 3}
+    # Dominante Farben in numerische Werte umwandeln und zur DataFrame hinzufügen
+    color_mapping = {'Rot': 0, 'Grün': 1, 'Blau': 2}
     labels_df['VibrantColor'] = labels_df['VibrantColor'].map(color_mapping)
 
-    # Add image dimensions to the dataframe
+    # Bild-Dimensionen hinzufügen
     labels_df['Width'] = dimensions_df['Width']
     labels_df['Height'] = dimensions_df['Height']
 
-    # Calculate aspect ratio
+    # Berechnung des Seitenverhältnisses
     labels_df['AspectRatio'] = labels_df['Width'] / labels_df['Height']
 
-    # Update the correlation matrix
+    # Korrelationsmatrix der Merkmale
     correlation = labels_df[['ClassId', 'VibrantColor', 'Width', 'Height', 'AspectRatio']].corr()
 
     plt.figure(figsize=(8, 6))
     sns.heatmap(correlation, annot=True, cmap='coolwarm', fmt=".2f")
-    plt.title('Correlation Matrix of Features Including Dominant Colors')
+    plt.title('Korrelationsmatrix der Merkmale')
+    plt.xlabel('Merkmale')
+    plt.ylabel('Merkmale')
+    plt.xticks(ticks=[0.5, 1.5, 2.5, 3.5, 4.5], labels=['Klassen-ID', 'Farbe', 'Breite', 'Höhe', 'Seitenverhältnis'])
+    plt.yticks(ticks=[0.5, 1.5, 2.5, 3.5, 4.5], labels=['Klassen-ID', 'Farbe', 'Breite', 'Höhe', 'Seitenverhältnis'])
     plt.show()
 
+# Extrahieren und Visualisieren von Farbhistogrammen (nur zur Veranschaulichung)
 def plot_color_histograms(labels_df, train_data_dir, test_data_dir):
     def extract_color_histogram(image, bins=(8, 8, 8)):
         image = image.convert('RGB')
@@ -297,7 +270,7 @@ def plot_color_histograms(labels_df, train_data_dir, test_data_dir):
         elif os.path.exists(img_path_test):
             full_img_path = img_path_test
         else:
-            print(f"Image not found: {img_path}")
+            print(f"Bild nicht gefunden: {img_path}")
             continue
         try:
             img = Image.open(full_img_path)
@@ -308,18 +281,19 @@ def plot_color_histograms(labels_df, train_data_dir, test_data_dir):
             plt.subplot(2, 5, idx+6)
             colors = ['r', 'g', 'b']
             for i, color in enumerate(colors):
-                plt.plot(hist[:, i].flatten(), color=color, label=f'{color.upper()} Channel')
-            plt.xlabel('Bin Number')
-            plt.ylabel('Frequency')
-            plt.title(f'Color Histogram - Class {sample_images.iloc[idx]["ClassId"]}')
+                plt.plot(hist[:, i].flatten(), color=color, label=f'{color.upper()} Kanal')
+            plt.xlabel('Bin-Nummer')
+            plt.ylabel('Häufigkeit')
+            plt.title(f'Farbhistogram - Klasse {sample_images.iloc[idx]["ClassId"]}')
             plt.legend(loc='upper right')
         except FileNotFoundError as e:
-            print(f"Error loading image: {e}")
+            print(f"Fehler beim Laden des Bildes: {e}")
             continue
 
     plt.tight_layout()
     plt.show()
 
+# Extrahieren und Visualisieren von Kantenbildern (nur zur Veranschaulichung)
 def plot_edges(labels_df, train_data_dir, test_data_dir):
     def extract_edges(image):
         image = image.convert('L')
@@ -338,7 +312,7 @@ def plot_edges(labels_df, train_data_dir, test_data_dir):
         elif os.path.exists(img_path_test):
             full_img_path = img_path_test
         else:
-            print(f"Image not found: {img_path}")
+            print(f"Bild nicht gefunden: {img_path}")
             continue
         try:
             img = Image.open(full_img_path)
@@ -346,30 +320,31 @@ def plot_edges(labels_df, train_data_dir, test_data_dir):
             plt.subplot(2, 5, idx+1)
             plt.imshow(img, cmap='gray')
             plt.axis('off')
-            plt.xlabel('Width')
-            plt.ylabel('Height')
+            plt.xlabel('Breite')
+            plt.ylabel('Höhe')
             plt.subplot(2, 5, idx+6)
             plt.imshow(edges, cmap='binary')
-            plt.title(f'Edges - Class {sample_images.iloc[idx]["ClassId"]}')
-            plt.xlabel('Width')
-            plt.ylabel('Height')
+            plt.title(f'Kanten - Klasse {sample_images.iloc[idx]["ClassId"]}')
+            plt.xlabel('Breite')
+            plt.ylabel('Höhe')
         except FileNotFoundError as e:
-            print(f"Error loading image: {e}")
+            print(f"Fehler beim Laden des Bildes: {e}")
             continue
 
     plt.tight_layout()
     plt.show()
 
+# Visualisierung von augmentierten Bildern (nur zur Veranschaulichung)
 def augment_and_plot_images(labels_df, train_data_dir, test_data_dir):
     datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        rotation_range = 12,    # Rotate images by max. 12 degrees
-        width_shift_range = 0.2,    # Shift images horizontally by max. 20%
-        height_shift_range = 0.2,   # Shift images vertically by max. 20%
-        shear_range = 0.1,  # Shear images by max. 20%
-        zoom_range = 0.25,  # Zoom images by max. 25%
-        horizontal_flip = False,    # Do not flip images horizontally
-        vertical_flip = False,  # Do not flip images vertically
-        fill_mode='nearest' # Fill in missing pixels with the nearest value
+        rotation_range = 12,    # Drehe Bilder zufällig um max. 12 Grad
+        width_shift_range = 0.2,    # Verschiebe Bilder horizontal um max. 20%
+        height_shift_range = 0.2,   # Verschiebe Bilder vertikal um max. 20%
+        shear_range = 0.1,  # Schere Bilder zufällig um max. 10%
+        zoom_range = 0.25,  # Zoome zufällig in Bilder hinein um max. 25%
+        horizontal_flip = False,    # Nicht horizontal spiegeln
+        vertical_flip = False,  # Nicht vertikal spiegeln
+        fill_mode='nearest' # Fülle leere Pixel mit den nächsten Nachbarn
     )
 
     sample_image_train_path = os.path.join(train_data_dir, labels_df.iloc[0]['Path'])
@@ -379,7 +354,7 @@ def augment_and_plot_images(labels_df, train_data_dir, test_data_dir):
     elif os.path.exists(sample_image_test_path):
         sample_image_path = sample_image_test_path
     else:
-        raise FileNotFoundError(f"Sample image not found in either directory.")
+        raise FileNotFoundError(f"Beispielbild in keinem der Verzeichnisse gefunden.")
 
     sample_image = np.array(Image.open(sample_image_path))
 
@@ -395,12 +370,12 @@ def augment_and_plot_images(labels_df, train_data_dir, test_data_dir):
     for i, aug_img in enumerate(augmented_images):
         plt.subplot(1, 6, i+2)
         plt.imshow(aug_img)
-        plt.title(f'Augmented {i+1}')
+        plt.title(f'Erweitert {i+1}')
         plt.axis('off')
 
     plt.tight_layout()
     plt.show()
 
-# Run the main function to execute the desired parts of the script
+# Hauptprogramm
 if __name__ == "__main__":
     main()
